7a31ef624cfa8b9c1a3fd45b3769c553ddae8bb6
Romasa102 <62591431+Romasa102@users.noreply.github.com>
2023-03-21 17:44:18 +0800

    English issue
    the sentence which we put into ChatGPT was crazy

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ pyautogui.typewrite('loading...')
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "user", "content": 'make this sentence formal student of this sentence: ' + clipboard_contents}])
+        {"role": "user", "content": 'make this sentence formal: ' + clipboard_contents}])
 for x in range(0, 10):
     keyboard.press_and_release('delete')
 res = []
